3b220fec462d92ac9d4630fdabef178567c752d1
LotharukpongJS <sodailot@gmail.com>
2023-09-01 10:23:50 +0200

    Added package description. `?phylomapr` to show package description.

---
+++ R/phylomapr-package.R
@@ -0,0 +1,14 @@
+#' phylomapr
+#' @title Quick retrieval of phylomaps
+#' @description
+#' Gene founder events facilitate evolutionary innovations. phylomapr enables quick retrieval of precomputed gene age maps (phylomaps) in R. Gene age maps loaded from phylomapr integrate seamlessly with myTAI.
+#'
+#' @docType package
+#' @name phylomapr
+#' @import assertthat
+#' @importFrom utils head tail
+#' @useDynLib phylomapr
+#' @source
+#' Lotharukpong (2023) _Unpublished_
+#' \url{https://lotharukpongjs.github.io/phylomapr/}
+NULL
